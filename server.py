24854d863f96a9f50a6a6dfaacb8d02b5943b9a2
Martin Løland <martin.loland@4subsea.com>
2020-03-10 15:08:23 +0100

    added templates

--- server.py
+++ server.py
@@ -1,24 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import json
 app = Flask(__name__)
 
-def get_users():
-    with open('users.json', 'r') as f:
-        return json.load(f)
-
-def write_users(users):
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-
-
 @app.route('/')
 def index():
-    return 'This is the homepage'
+    users = get_users()
+    return render_template('index.html', users=users)
 
 @app.route('/api/users')
 def api_users():
-    with open('users.json', 'r') as f:
-        users = json.load(f)
+    users = get_users()
     if not users:
         return 'There are no users defined'
     return jsonify(users)
@@ -31,3 +22,12 @@ def api_users_add(username):
     users.append(username)
     write_users(users)
     return '{} added to users'.format(username)
+
+
+def get_users():
+    with open('users.json', 'r') as f:
+        return json.load(f)
+
+def write_users(users):
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
