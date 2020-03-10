f7d90ab6594fd4c7cb56e7c5ced88b92e62e4d8a
Martin Løland <martin.loland@4subsea.com>
2020-03-10 14:58:43 +0100

    lint

--- server.py
+++ server.py
@@ -2,23 +2,13 @@ from flask import Flask, jsonify
 import json
 app = Flask(__name__)
 
-def get_users():
-    with open('users.json', 'r') as f:
-        return json.load(f)
-
-def write_users(users):
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-
-
 @app.route('/')
 def index():
     return 'This is the homepage'
 
 @app.route('/api/users')
 def api_users():
-    with open('users.json', 'r') as f:
-        users = json.load(f)
+    users = get_users()
     if not users:
         return 'There are no users defined'
     return jsonify(users)
@@ -31,3 +21,12 @@ def api_users_add(username):
     users.append(username)
     write_users(users)
     return '{} added to users'.format(username)
+
+
+def get_users():
+    with open('users.json', 'r') as f:
+        return json.load(f)
+
+def write_users(users):
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
