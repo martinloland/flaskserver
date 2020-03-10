b8d88867b92c1711b34724068e18a5d69aca4da4
Martin Løland <martin.loland@4subsea.com>
2020-03-10 14:56:23 +0100

    added users api

--- server.py
+++ server.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 app = Flask(__name__)
+
+def get_users():
+    with open('users.json', 'r') as f:
+        return json.load(f)
+
+def write_users(users):
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'This is the homepage'
+
+@app.route('/api/users')
+def api_users():
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+    if not users:
+        return 'There are no users defined'
+    return jsonify(users)
+
+@app.route('/api/users/add/<username>')
+def api_users_add(username):
+    users = get_users()
+    if username in users:
+        return '{} already in users'.format(username)
+    users.append(username)
+    write_users(users)
+    return '{} added to users'.format(username)
